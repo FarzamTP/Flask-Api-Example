678d8a8285a9780f1ca1be3832b23a41bb450a91
FarzamTP <farzam.taghipour@gmail.com>
2022-01-24 22:05:48 +0100

    Single variable added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask import render_template
 from scipy.optimize import linprog
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable, LpMinimize
 
@@ -30,22 +32,22 @@ def maximize_problem_solve():
     status = model.solve()
 
     # Get the results
-    print(f"status: {model.status}, {LpStatus[model.status]}")
-    print(f"objective: {model.objective.value()}")
+    # print(f"status: {model.status}, {LpStatus[model.status]}")
+    # print(f"objective: {model.objective.value()}")
 
     variables = {}
     constants = {}
 
     for var in x.values():
-        print(f"{var.name}: {var.value()}")
+        # print(f"{var.name}: {var.value()}")
         variables[str(var.name)] = var.value()
 
     for name, constraint in model.constraints.items():
-        print(f"{name}: {constraint.value()}")
+        # print(f"{name}: {constraint.value()}")
         constants[str(name)] = constraint.value()
 
-    print(variables)
-    print(constants)
+    # print(variables)
+    # print(constants)
 
     return {"status": str(model.status) + ", " + str(LpStatus[model.status]),
             "optimum-value": model.objective.value(),
@@ -76,24 +78,67 @@ def minimize_problem_solve():
     model.solve()
 
     # Get the results
-    print(f"status: {model.status}, {LpStatus[model.status]}")
-    print(f"objective: {model.objective.value()}")
+    # print(f"status: {model.status}, {LpStatus[model.status]}")
+    # print(f"objective: {model.objective.value()}")
 
     variables = {}
     constants = {}
 
     for var in x.values():
-        print(f"{var.name}: {var.value()}")
+        # print(f"{var.name}: {var.value()}")
         variables[str(var.name)] = var.value()
 
     for name, constraint in model.constraints.items():
-        print(f"{name}: {constraint.value()}")
+        # print(f"{name}: {constraint.value()}")
         constants[str(name)] = constraint.value()
 
     return {"status": str(model.status) + ", " + str(LpStatus[model.status]),
             "optimum-value": model.objective.value(),
             "variables-opt-value": variables,
             "constants-value": constants}
+
+
+# =============================== Solve one-variable LP =============================
+
+@app.route('/solve_one_variable', methods=['POST', 'GET'])
+def one_variable_solver():
+    var1 = np.random.randint(100, 500)
+    c1 = np.random.randint(-10, 10)
+    var2 = np.random.randint(100, 300)
+    c2 = np.random.randint(-10, 10)
+    var3 = np.random.randint(-100, 100)
+    c3 = np.random.randint(-10, 10)
+    var4 = np.random.randint(-200, 500)
+    c4 = np.random.randint(-10, 10)
+    c5 = np.random.randint(-10, 10)
+
+    constant = c1 * var1 + c2 * var2 + c3 * var3 + c4 * var4
+    x_space = np.linspace(-10, 10)
+
+    y_init = constant + c5 * x_space
+
+    x_target = np.round(np.random.uniform(1, 10), 2)
+    y_target = constant + c5 * x_target
+
+    x1, x2 = 2, 3
+    y1 = constant + c5 * x1
+    y2 = constant + c5 * x2
+
+    slope = (y2 - y1) / (x2 - x1)
+
+    x_guess = np.round((y_target - constant) / slope, 2)
+
+    plt.plot(x_target, y_target, 'go', label="target value")
+    plt.plot(x_space, y_init, 'r--', label="Truth Line")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.savefig('./static/images/new_plot.png')
+    plt.legend()
+    plt.show()
+
+    return render_template('index.html',
+                           name=f'Exact x value: {float(x_target)}. X guess: {float(x_guess)} the value of y is set to {float(y_target)}.',
+                           url='./static/images/new_plot.png')
 
 
 if __name__ == '__main__':
